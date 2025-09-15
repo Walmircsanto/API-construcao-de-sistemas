2689282eccd5c0a6f64f37f9fb1fe8d1dbd58a14
walmir.rosa <walmirsantosrosa@gmail.com>
2025-09-15 07:49:22 -0300

    first commit

---
+++ src/main/java/br/com/construcao/sistemas/ContrucaoSistemasApplication.java
@@ -0,0 +1,13 @@
+package br.com.construcao.sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrucaoSistemasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContrucaoSistemasApplication.class, args);
+    }
+
+}

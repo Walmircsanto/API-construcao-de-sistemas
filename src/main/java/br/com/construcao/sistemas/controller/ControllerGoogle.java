0825d54da5b57774769ba061c8a8a7aeafb3e63b
Walmircsanto <walmirsantosrosa@gmail.com>
2025-11-17 13:59:12 -0300

    :sparkles: feat: Adicionar controller para redirecionamento de log com o google

--- src/main/java/br/com/construcao/sistemas/controller/ControllerGoogle.java
+++ src/main/java/br/com/construcao/sistemas/controller/ControllerGoogle.java
@@ -3,6 +3,7 @@ package br.com.construcao.sistemas.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControllerGoogle {
 
     @GetMapping("google/home")
-    public ResponseEntity<String> getGoogle() {
-        return ResponseEntity.ok("Google");
+    public ResponseEntity<String> getGoogle(@RequestParam(required = false) String token,
+                                           @RequestParam(required = false) String user) {
+        if (token != null && user != null) {
+            return ResponseEntity.ok(String.format(
+                "Login OAuth2 realizado com sucesso!\nUsuário: %s\nToken JWT: %s", 
+                user, token
+            ));
+        }
+        return ResponseEntity.ok("Login OAuth2 realizado com sucesso!");
+    }
+    
+    @GetMapping("google/error")
+    public ResponseEntity<String> getGoogleError(@RequestParam(required = false) String reason) {
+        String message = "Erro no login OAuth2 com Google";
+        if (reason != null) {
+            message += ": " + reason;
+        }
+        return ResponseEntity.badRequest().body(message);
     }
 }

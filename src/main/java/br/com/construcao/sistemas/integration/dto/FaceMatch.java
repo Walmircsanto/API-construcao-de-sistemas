ff2d56bc5d6550ed04d3961f64e494863fd09e11
Walmircompass <walmir.rosa@oncoclinicas.com>
2025-12-07 21:37:09 -0300

    feat: Adicionar novo atributo nos FaceMath

--- src/main/java/br/com/construcao/sistemas/integration/dto/FaceMatch.java
+++ src/main/java/br/com/construcao/sistemas/integration/dto/FaceMatch.java
@@ -8,5 +8,10 @@ public class FaceMatch {
     private Long suspect_id;
     private Double distance;
     private String metadata;
-    private String s3_path;
+    private String s3_path; // esse e meu original_url
+    private String processed_url;
+
+    private String detection_location;
+    private String detection_date;
+    private String detection_time;
 }
